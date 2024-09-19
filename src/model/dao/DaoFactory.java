057635e584a4ef49841e437babf89f887f1bcb26
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-09-19 17:19:38 -0300

    DepartmentDao finalizado

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -10,5 +10,7 @@ public class DaoFactory {
         return new SellerDaoJDBC(DB.getConnection());
     }
 
-    
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
+    }
 }
